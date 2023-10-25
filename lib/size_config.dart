aeb55acf50006b92eaa0dab2f250145fa6cbbdaa
SuyogRedkar <76471004+SuyogRedkar@users.noreply.github.com>
2023-10-25 11:33:13 +0530

    Update size_config.dart

--- lib/size_config.dart
+++ lib/size_config.dart
@@ -17,13 +17,13 @@ class SizeConfig {
 
 double getProportionateScreenHeight(double inputHeight) {
   double screenHeight = SizeConfig.screenHeight;
-  // 812 is the layout height that designer use
+
   return (inputHeight / 800.0) * screenHeight;
 }
 
-// Get the proportionate height as per screen size
+
 double getProportionateScreenWidth(double inputWidth) {
   double screenWidth = SizeConfig.screenWidth;
-  // 375 is the layout width that designer use
+
   return (inputWidth / 360.0) * screenWidth;
 }
